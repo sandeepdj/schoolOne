822f39424c448c86c41fc9591613bfae9d38abcc
sandeepraodj <sandeepraodj@gmail.com>
2018-05-14 22:31:07 +0530

    Login page design started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { AuthComponent } from './shared/components/auth/auth.component';
+
 const routes: Routes = [
   {
     path: '',
+    component: AuthComponent,
     children: []
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +24,7 @@ import { DashboardComponent } from './core/components/dashboard/dashboard.compon
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
